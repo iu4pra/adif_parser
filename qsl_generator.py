747902fade232ba36b9c0b22959954b3653d8326
Federico Bacilieri <you@example.com>
2025-10-15 14:57:02 +0200

    Added wkhtmltopdf call, refactoring

--- qsl_generator.py
+++ qsl_generator.py
@@ -5,6 +5,8 @@
 # wkhtmltox reference https://wkhtmltopdf.org/downloads.html
 from jinja2 import Environment, FileSystemLoader
 import os.path
+import pypdf
+import subprocess
 
 # Loading the environment
 env = Environment(loader=FileSystemLoader('.'))
@@ -13,13 +15,19 @@ env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template('template.html')
 
 # Rendering the template and storing the resulting text in variable output
-output = template.render(qso={'call': 'IZ4HUF', 'band': '12M', 'freq': '14080', 'rst_sent': 599})
+output = template.render(
+    qso={'call': 'IZ4HUF', 'band': '12M', 'freq': '14.080', 'rst_sent': 599})
 
-# Delete previous file
-TEMPLATE_OUT_FILENAME = './template_out.html'
-if os.path.isfile(TEMPLATE_OUT_FILENAME):
-    os.unlink(TEMPLATE_OUT_FILENAME)
+# Delete previous files
+TEMPLATE_TEMP_FILENAME = './template_out.html'
+PDF_OUTPUT = './out.pdf'
+if os.path.isfile(TEMPLATE_TEMP_FILENAME):
+    os.unlink(TEMPLATE_TEMP_FILENAME)
 
 # Write to file
-with open(TEMPLATE_OUT_FILENAME, 'wt', encoding='utf-8') as f:
+with open(TEMPLATE_TEMP_FILENAME, 'wt', encoding='utf-8') as f:
     f.write(output)
+
+# Convert single page to PDF
+subprocess.run(["./wkhtmltopdf", "--page-width", "14cm",
+               "--page-height", "9cm", TEMPLATE_TEMP_FILENAME, PDF_OUTPUT])
