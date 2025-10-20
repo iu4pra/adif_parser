fa185dffe57984056b959f14e176b5a4381f62f0
Federico Bacilieri <you@example.com>
2025-10-20 07:32:44 +0200

    Functionized

--- qsl_generator.py
+++ qsl_generator.py
@@ -30,6 +30,38 @@ def dict_to_cmd_list(_cmd_options: dict):
     return cmd_list
 
 
+def generate_qsl_pdf(qso_list: list):
+    """Generates a PDF file qith the QSLs contained in the given QSO list"""
+    assert isinstance(qso_list, list(QSO({})))
+    for i, _qso in enumerate(qso_list):
+        assert isinstance(_qso, QSO)
+        # Rendering the template and storing the resulting text in variable output
+        qso_data_lowercase = {}
+        for key, value in _qso._d.items():
+            qso_data_lowercase[key.casefold()] = value
+        output = template.render(qso=qso_data_lowercase)
+
+        # Write compiled template to file
+        with open(TEMPLATE_TEMP_FILENAME, 'wt', encoding='utf-8') as f:
+            f.write(output)
+
+        # Convert template page to PDF
+        subprocess.run(["./wkhtmltopdf"] + dict_to_cmd_list(cmd_options) +
+                       [TEMPLATE_TEMP_FILENAME, (PDF_TEMP_BASE_NAME % i)])
+
+    writer = pypdf.PdfWriter()
+    for pdf in [(PDF_TEMP_BASE_NAME % i) for i in range(len(qso_list))]:
+        writer.append(pdf)
+    writer.write(PDF_OUTPUT)
+    writer.close()
+
+    # Delete temporary files
+    unlink_if_exists(TEMPLATE_TEMP_FILENAME)
+    # Delete qso_*.pdf
+    result = subprocess.run(
+        f"rm -fv {PDF_TEMP_BASE_NAME.replace('%04d','*')}", shell=True)
+
+
 # QSO list
 # TODO dumpfile for testing only!
 QSO_DUMP_FILE = './sample_log.dump'
@@ -60,30 +92,4 @@ template = env.get_template('template.html')
 with open(QSO_DUMP_FILE, 'rb') as f:
     qso_list = pickle.load(f)
 
-for i, _qso in enumerate(qso_list):
-    assert isinstance(_qso, QSO)
-    # Rendering the template and storing the resulting text in variable output
-    qso_data_lowercase = {}
-    for key, value in _qso._d.items():
-        qso_data_lowercase[key.casefold()] = value
-    output = template.render(qso=qso_data_lowercase)
-
-    # Write compiled template to file
-    with open(TEMPLATE_TEMP_FILENAME, 'wt', encoding='utf-8') as f:
-        f.write(output)
-
-    # Convert template page to PDF
-    subprocess.run(["./wkhtmltopdf"] + dict_to_cmd_list(cmd_options) +
-                   [TEMPLATE_TEMP_FILENAME, (PDF_TEMP_BASE_NAME % i)])
-
-writer = pypdf.PdfWriter()
-for pdf in [(PDF_TEMP_BASE_NAME % i) for i in range(len(qso_list))]:
-    writer.append(pdf)
-writer.write(PDF_OUTPUT)
-writer.close()
-
-# Delete temporary files
-unlink_if_exists(TEMPLATE_TEMP_FILENAME)
-# Delete qso_*.pdf
-result = subprocess.run(
-    f"rm -fv {PDF_TEMP_BASE_NAME.replace('%04d','*')}", shell=True)
+generate_qsl_pdf(qso_list)
