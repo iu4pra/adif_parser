067790e5f599ba9f5500dcee0a5e3cf0df1d7d9c
Federico Bacilieri <federico.bacilieri@gmail.com>
2025-10-22 18:34:51 +0200

    Module created

---
+++ wkhtml.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+# This software under the MIT License
+# wkhtmltox wrapper
+
+import os
+import platform
+import subprocess
+
+def wkhtmltoimage(args: list = []):
+    """Invokes wkhtmltoimage with the given arguments"""
+    _os = platform.uname()[0]
+    if _os == 'Windows':
+        subprocess.run(["./wkhtmltoimage.exe"] + args)
+    elif _os == 'Linux':
+        subprocess.run(["./wkhtmltoimage"] + args)
+    return NotImplementedError("Not implemented for the given OS")
+
+def wkhtmltopdf(args: list = []):
+    """Invokes wkhtmltopdf with the given arguments"""
+    _os = platform.uname()[0]
+    if _os == 'Windows':
+        subprocess.run(["./wkhtmltopdf.exe"] + args)
+    elif _os == 'Linux':
+        subprocess.run(["./wkhtmltopdf"] + args)
+    return NotImplementedError("Not implemented for the given OS")
+
+wkhtmltoimage()
+wkhtmltopdf()
