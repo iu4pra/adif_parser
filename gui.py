19cc6ded9256733d49cf8ea3263b4a50d4bf8c5b
Federico Bacilieri <you@example.com>
2025-11-03 14:33:34 +0100

    Refactoring + default values

--- gui.py
+++ gui.py
@@ -22,11 +22,12 @@ class App():
         self.checkbox_frame.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
 
         self.out_pdf = tk.IntVar()
-        self.out_img = tk.IntVar()
+        self.out_pdf.set(1)
         c_pdf = tk.Checkbutton(self.checkbox_frame, text="PDF Output", variable=self.out_pdf,
                                onvalue=1, offvalue=0)
         c_pdf.grid(row=0, column=0)
 
+        self.out_img = tk.IntVar()
         c_img = tk.Checkbutton(self.checkbox_frame, text="Image Output",
                                variable=self.out_img, onvalue=1, offvalue=0)
         c_img.grid(row=1, column=0)
