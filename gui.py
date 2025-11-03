6169052051fc40cee2f5190b4794a5e4b900daa0
Federico Bacilieri <federico.bacilieri@gmail.com>
2025-11-03 18:00:15 +0100

    Log box added

--- gui.py
+++ gui.py
@@ -4,10 +4,36 @@
 # GUI for QSL generator written in Tkinter
 
 import adif
-
 import qsl_generator
+
+import logging
 import tkinter as tk
 import tkinter.filedialog as tkfile
+import tkinter.scrolledtext as tkscroll
+
+
+class TextHandler(logging.Handler):
+    # This class allows you to log to a Tkinter Text or ScrolledText widget
+    # Adapted from Moshe Kaplan: https://gist.github.com/moshekaplan/c425f861de7bbf28ef06
+    # Source: https://stackoverflow.com/questions/13318742/python-logging-to-tkinter-text-widget
+
+    def __init__(self, text):
+        # run the regular Handler __init__
+        logging.Handler.__init__(self)
+        # Store a reference to the Text it will log to
+        self.text = text
+
+    def emit(self, record):
+        msg = self.format(record)
+
+        def append():
+            self.text.configure(state='normal')
+            self.text.insert(tk.END, msg + '\n')
+            self.text.configure(state='disabled')
+            # Autoscroll to the bottom
+            self.text.yview(tk.END)
+        # This is necessary because we can't modify the Text from other threads
+        self.text.after(0, append)
 
 
 class App():
@@ -55,6 +81,16 @@ class App():
         self.quit_button = tk.Button(self.buttons_frame, text="Quit")
         self.quit_button['command'] = master.destroy
         self.quit_button.grid(row=1, column=2, padx=5, pady=5)
+
+        # Logging
+        self.logbox = tkscroll.ScrolledText(master, state='disabled')
+        self.logbox.grid(row=2, column=0, columnspan=3)
+        
+        logger = logging.getLogger('app')
+        log_handler = TextHandler(self.logbox)
+        log_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
+        logger.addHandler(log_handler)
+        logger.setLevel(logging.INFO)
 
     def logfile_chooser(self):
         """Open file chooser for log file"""
