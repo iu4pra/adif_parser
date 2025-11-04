7dca69aad3214c918ae8063c6ba1019df04e800a
Federico Bacilieri <you@example.com>
2025-11-04 08:05:20 +0100

    Formatting

--- wkhtml.py
+++ wkhtml.py
@@ -15,11 +15,9 @@ def wkhtmltoimage(args: list = []):
     """Invokes wkhtmltoimage with the given arguments"""
     _os = platform.uname()[0]
     if _os == 'Windows':
-        return subprocess.run(
-            [os.path.join(WKHTMLTOX_BASE_PATH, "wkhtmltoimage.exe")] + args, capture_output=True, text=True)
+        return subprocess.run([os.path.join(WKHTMLTOX_BASE_PATH, "wkhtmltoimage.exe")] + args, capture_output=True, text=True)
     elif _os == 'Linux':
-        return subprocess.run(
-            [os.path.join(WKHTMLTOX_BASE_PATH, "wkhtmltoimage")] + args, capture_output=True, text=True)
+        return subprocess.run([os.path.join(WKHTMLTOX_BASE_PATH, "wkhtmltoimage")] + args, capture_output=True, text=True)
     else:
         raise NotImplementedError("Not implemented for the given OS")
 
@@ -28,10 +26,8 @@ def wkhtmltopdf(args: list = []):
     """Invokes wkhtmltopdf with the given arguments"""
     _os = platform.uname()[0]
     if _os == 'Windows':
-        return subprocess.run(
-            [os.path.join(WKHTMLTOX_BASE_PATH, "wkhtmltopdf.exe")] + args, capture_output=True, text=True)
+        return subprocess.run([os.path.join(WKHTMLTOX_BASE_PATH, "wkhtmltopdf.exe")] + args, capture_output=True, text=True)
     elif _os == 'Linux':
-        return subprocess.run(
-            [os.path.join(WKHTMLTOX_BASE_PATH, "wkhtmltopdf")] + args, capture_output=True, text=True)
+        return subprocess.run([os.path.join(WKHTMLTOX_BASE_PATH, "wkhtmltopdf")] + args, capture_output=True, text=True)
     else:
         raise NotImplementedError("Not implemented for the given OS")
